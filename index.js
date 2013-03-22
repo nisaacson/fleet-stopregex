0282b4aa8de772bd3f7ad431e6de488da725a15b
Noah Isaacson <noah@nisaacson.com>
2013-03-22 03:08:58 -0400

    remove should dep

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var prompt = require('prompt');
 var async = require('async')
-var should = require('should');
 var getPSJson = require('fleet-ps-json')
 var getPSText = require('./getPSText')
 var opts = require('optimist').usage('Usage:  --field [command] $0, where command is one of "pid", "commit", or "command"').alias('f','field')
